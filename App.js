93bfb29f73f63ffce7da9b923033ae8ad511713f
Manuel Desole <desole.manuel@gmail.com>
2021-04-22 22:22:46 +0200

    AccountScreen

--- App.js
+++ App.js
@@ -8,7 +8,11 @@ import AppCard from "./app/components/AppCard";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
+import Screen from "./app/screens/Screen";
+import Icon from "./app/components/Icon";
+import ListItem from "./app/components/ListItem";
+import AccountScreen from "./app/screens/AccountScreen";
 
 export default function App() {
-  return <MessagesScreen />;
+  return <AccountScreen />;
 }
